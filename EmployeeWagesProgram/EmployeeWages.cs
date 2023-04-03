4ae5f2ef120d108f2838cb035db2a330ead42059
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 22:33:26 +0530

    Calculating Employee PartTime wages

--- EmployeeWagesProgram/EmployeeWages.cs
+++ EmployeeWagesProgram/EmployeeWages.cs
@@ -12,14 +12,19 @@ namespace EmployeeWagesProgram
         {
             int Emp_Present = 1;
             int Emp_Rate_Per_Hour = 20;
+            int Is_Part_Time = 2;
             int empHrs = 0;
             int EmpWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
             if (empCheck == Emp_Present)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == Is_Part_Time)
+            {
+                empHrs = 4;
             }
             else
             {
