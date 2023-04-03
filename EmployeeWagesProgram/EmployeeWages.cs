1c4d3aae1392bc55874016dedeb4189e80ac32b7
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 22:51:29 +0530

    Calculating Employee Wages for month

--- EmployeeWagesProgram/EmployeeWages.cs
+++ EmployeeWagesProgram/EmployeeWages.cs
@@ -8,30 +8,37 @@ namespace EmployeeWagesProgram
 {
     public class EmployeeWages
     {
+        public const int Num_of_Working_Days = 20;
         public static void CheckingEmployeeWages()
         {
             int Emp_Present = 1;
-            int Emp_Rate_Per_Hour = 20;
+            int Emp_Rate_Per_Hour=20;
             int Is_Part_Time = 2;
             int empHrs = 0;
             int EmpWage = 0;
+            int TotalEmpWage = 0;
 
-            Random random = new Random();
-            int empCheck = random.Next(3);
-            switch (empCheck)
+            for (int day = 0; day < Num_of_Working_Days; day++)
             {
-                case 0:
-                    empHrs = 8;
-                    break;
-                case 1:
-                    empHrs = 4;
-                    break;
-                case 2:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case 0:
+                        empHrs = 8;
+                        break;
+                    case 1:
+                        empHrs = 4;
+                        break;
+                    case 2:
+                        empHrs = 0;
+                        break;
+                }
+                EmpWage = empHrs * Emp_Rate_Per_Hour;
+                TotalEmpWage += EmpWage;
+                Console.WriteLine("Emp Wage: " + EmpWage);
             }
-            EmpWage = empHrs * Emp_Rate_Per_Hour;
-            Console.WriteLine("Emp Wage: " + EmpWage);
+            Console.WriteLine("Toayal Emp Wage: " + TotalEmpWage);
         }
     }
 }
