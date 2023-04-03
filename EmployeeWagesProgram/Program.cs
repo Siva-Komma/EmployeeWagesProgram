a372a5c523eb7e7bd83cc24523b81b7bbbaa5506
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 22:00:07 +0530

    Displaying Welcome Message of Employee Wages Program

---
+++ EmployeeWagesProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWagesProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wages Program");
+            Console.ReadLine();
+        }
+    }
+}
