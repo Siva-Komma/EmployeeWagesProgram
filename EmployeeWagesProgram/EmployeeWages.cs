f14ce715daba895b1213716f8f57566c62c3c09f
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 23:02:39 +0530

    Calculating total monthly salary based on employee working hours

--- EmployeeWagesProgram/EmployeeWages.cs
+++ EmployeeWagesProgram/EmployeeWages.cs
@@ -9,16 +9,19 @@ namespace EmployeeWagesProgram
     public class EmployeeWages
     {
         public const int Num_of_Working_Days = 20;
+        public const int Max_Hrs_In_Month = 100;
         public static void CheckingEmployeeWages()
         {
             int Emp_Present = 1;
-            int Emp_Rate_Per_Hour=20;
+            int Emp_Rate_Per_Hour = 20;
             int Is_Part_Time = 2;
             int empHrs = 0;
             int EmpWage = 0;
             int TotalEmpWage = 0;
+            int TotalEmpHrs = 0;
 
-            for (int day = 0; day < Num_of_Working_Days; day++)
+
+            while (TotalEmpHrs < Max_Hrs_In_Month && Num_of_Working_Days <= Num_of_Working_Days)
             {
                 Random random = new Random();
                 int empCheck = random.Next(3);
@@ -34,10 +37,13 @@ namespace EmployeeWagesProgram
                         empHrs = 0;
                         break;
                 }
-                EmpWage = empHrs * Emp_Rate_Per_Hour;
+                EmpWage = Emp_Rate_Per_Hour * empHrs;
                 TotalEmpWage += EmpWage;
+                TotalEmpHrs += empHrs;
+                Console.WriteLine("Emp Hrs: " + TotalEmpHrs);
                 Console.WriteLine("Emp Wage: " + EmpWage);
             }
+            TotalEmpWage = TotalEmpHrs * Emp_Rate_Per_Hour;
             Console.WriteLine("Toayal Emp Wage: " + TotalEmpWage);
         }
     }
